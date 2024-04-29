f36a3374459debb5970d91e590f0c331c3ac77fa
ZyonAytkyn <zionaitken@gmail.com>
2024-04-29 17:44:24 +0100

    Initial Commit
    An initial commit of the finished project files.

---
+++ Source/StarvingSeagulls/StarvingSeagulls.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StarvingSeagulls.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StarvingSeagulls, "StarvingSeagulls" );
---
+++ Source/StarvingSeagulls/StarvingSeagullsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/GameEngine.h"
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StarvingSeagullsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARVINGSEAGULLS_API AStarvingSeagullsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
